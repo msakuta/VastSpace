ddeed6c2edf4387c6565f48f6076aaa60caee4ed
msakuta <masahiro.sakuta@gmail.com>
2010-04-19 19:01:48 +0000

    (+) Added ring's lighting to Saturn. It's completed in the fragment shader itself. It became real ever but may cost GPU power.
    git-svn-id: svn://localhost/gltestplus/trunk@245 42c370b9-4569-d348-85c3-ab3aefc53057

--- shaders/ringsphereshadow.fs
+++ shaders/ringsphereshadow.fs
@@ -25,11 +25,17 @@ void main (void)
 	vec3 lnorm = gl_NormalMatrix * ringnorm;
 
 	// Calculate projected point onto ring plane.
-	vec3 q = pos - dot(pos, lnorm) / dot(light, lnorm) * light;
+	float normp = dot(pos, lnorm);
+	float lightp = dot(light, lnorm);
+	vec3 q = pos - normp / lightp * light;
+
+	float temp = 1 - abs(dot(npos, lnorm));
+	float ramb = 5 * min(1., 1 + dot(pos, light)) * (normp * lightp < 0 ? .1 : .8) * abs(lightp) * abs(normp) * temp * temp;
 
 	// Blend texturing, lighting and ring shadowing together.
 	gl_FragColor = textureCube(tex, vec3(gl_TexCoord[0]))
-		* (gl_FrontLightProduct[0].ambient + globalAmbient
+		* (ramb + 
+		gl_FrontLightProduct[0].ambient + globalAmbient
 		+ gl_FrontLightProduct[0].diffuse
 		* max(lightangle * (0. < dot(q, light) ? texture1D(tex1d, (length(q) - ringmin) / (ringmax - ringmin)) : vec4(1)), 0.));
 }

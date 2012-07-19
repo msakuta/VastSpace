3b78ef8d562c2c902ef5d2405c2799c93b6633c6
msakuta <masahiro.sakuta@gmail.com>
2012-07-19 17:33:04 +0000

    Made GLWchart capable of adding and deleting arbitrary series from the right-click context menu.
    git-svn-id: svn://localhost/gltestplus/branches/bullet@831 42c370b9-4569-d348-85c3-ab3aefc53057

--- src/glw/GLWchart.cpp
+++ src/glw/GLWchart.cpp
@@ -15,97 +15,6 @@
 #include <GL/gl.h>
 
 
-static sqa::Initializer definer("GLWchart", GLWchart::sq_define);
-
-
-GLWchart::GLWchart(const char *title, ChartSeries *_series) : st(title){
-	flags = GLW_CLOSE | GLW_COLLAPSABLE;
-	if(_series)
-		series.push_back(_series);
-}
-
-int GLWchart::mouse(GLwindowState &ws, int button, int state, int x, int y){
-	struct PopupMenuItemToggleShow : public PopupMenuItem{
-		typedef PopupMenuItem st;
-		WeakPtr<GLWchart> p;
-		int index;
-		PopupMenuItemToggleShow(gltestp::dstring title, GLWchart *p, int index) : st(title), p(p), index(index){}
-		PopupMenuItemToggleShow(const PopupMenuItemToggleShow &o) : st(o), p(o.p), index(o.index){}
-		virtual void execute(){
-			if(p)
-				p->setSeriesVisible(index, Toggle);
-		}
-		virtual PopupMenuItem *clone(void)const{return new PopupMenuItemToggleShow(*this);}
-	};
-
-	if(button == GLUT_RIGHT_BUTTON && state == GLUT_UP){
-		PopupMenu pm;
-		for(int i = 0; i < series.size(); i++)
-			pm.append(new PopupMenuItemToggleShow(gltestp::dstring(series[i]->visible ? "Hide " : "Show ") << series[i]->labelname(), this, i));
-
-		glwPopupMenu(ws, pm);
-	}
-	return 1;
-}
-
-void GLWchart::setSeriesVisible(int i, FlagMod v){
-	if(i < 0 || series.size() <= i)
-		return;
-	series[i]->visible = !!(v & (1 << !!series[i]->visible));
-}
-
-void GLWchart::anim(double dt){
-	for(int i = 0; i < series.size(); i++){
-		if(series[i])
-			series[i]->anim(dt, this);
-	}
-}
-
-void GLWchart::draw(GLwindowState &ws, double t){
-	for(int i = 0; i < series.size(); i++){
-		ChartSeries *cs = series[i];
-		if(cs && cs->visible){
-			GLWrect cr = clientRect();
-
-			glColor4fv(cs->color());
-			glwpos2d(cr.x0, cr.y0 + (i + 2) * GLwindow::glwfontwidth * glwfontscale);
-			glwprintf(cs->labelstr());
-			glBegin(GL_LINE_STRIP);
-			int all = cs->count();
-			for(int i = 0; i < all; i++){
-				glVertex2d(cr.x0 * (all - i - 1) / all + cr.x1 * i / all, cr.y1 - (cr.height() - 2) * cs->value(i));
-			}
-			glEnd();
-		}
-	}
-}
-
-/// Define class GLWmessage for Squirrel
-bool GLWchart::sq_define(HSQUIRRELVM v){
-	st::sq_define(v);
-	sq_pushstring(v, _SC("GLWchart"), -1);
-	sq_pushstring(v, _SC("GLwindow"), -1);
-	sq_get(v, -3);
-	sq_newclass(v, SQTrue);
-	sq_settypetag(v, -1, "GLWchart");
-	sq_setclassudsize(v, -1, sizeof(WeakPtr<GLelement>));
-	register_closure(v, _SC("constructor"), sqf_constructor);
-	register_closure(v, _SC("addSeries"), sqf_addSeries);
-	sq_createslot(v, -3);
-	return true;
-}
-
-/// Squirrel constructor
-SQInteger GLWchart::sqf_constructor(HSQUIRRELVM v){
-	SQInteger argc = sq_gettop(v);
-
-	GLWchart *p = new GLWchart("Chart");
-
-	sq_assignobj(v, p);
-	glwAppend(p);
-	return 0;
-}
-
 class FrameTimeChartSeries : public GLWchart::TimeChartSeries{
 	virtual gltestp::dstring labelname()const{return "Frame Time";}
 	virtual double timeProc(double dt){return dt;}
@@ -174,6 +83,138 @@ class RecvBytesHistogramChartSeries : public HistogramChartSeries{
 	virtual Vec4f color()const{return Vec4f(1,1,0,1);}
 	virtual gltestp::dstring labelname()const{return "Received bytes histogram";}
 };
+
+
+static sqa::Initializer definer("GLWchart", GLWchart::sq_define);
+
+
+GLWchart::GLWchart(const char *title, ChartSeries *_series) : st(title){
+	flags = GLW_CLOSE | GLW_COLLAPSABLE;
+	if(_series)
+		series.push_back(_series);
+}
+
+template<typename T>
+struct PopupMenuItemAddSeries : public PopupMenuItem{
+	typedef PopupMenuItem st;
+	typedef PopupMenuItemAddSeries tt;
+	WeakPtr<GLWchart> p;
+	tt(gltestp::dstring title, GLWchart *p) : st(title), p(p){}
+	tt(const tt &o) : st(o), p(o.p){}
+	virtual void execute(){
+		if(p)
+			p->addSeries(new T());
+	}
+	virtual PopupMenuItem *clone(void)const{return new tt(*this);}
+};
+
+int GLWchart::mouse(GLwindowState &ws, int button, int state, int x, int y){
+	struct PopupMenuItemToggleShow : public PopupMenuItem{
+		typedef PopupMenuItem st;
+		WeakPtr<GLWchart> p;
+		int index;
+		PopupMenuItemToggleShow(gltestp::dstring title, GLWchart *p, int index) : st(title), p(p), index(index){}
+		PopupMenuItemToggleShow(const PopupMenuItemToggleShow &o) : st(o), p(o.p), index(o.index){}
+		virtual void execute(){
+			if(p)
+				p->setSeriesVisible(index, Toggle);
+		}
+		virtual PopupMenuItem *clone(void)const{return new PopupMenuItemToggleShow(*this);}
+	};
+
+	struct PopupMenuItemDelete : public PopupMenuItem{
+		typedef PopupMenuItem st;
+		typedef PopupMenuItemDelete tt;
+		WeakPtr<GLWchart> p;
+		int index;
+		tt(gltestp::dstring title, GLWchart *p, int index) : st(title), p(p), index(index){}
+		tt(const tt &o) : st(o), p(o.p), index(o.index){}
+		virtual void execute(){
+			if(p){
+				delete p->series[index];
+				p->series.erase(p->series.begin() + index);
+			}
+		}
+		virtual PopupMenuItem *clone(void)const{return new tt(*this);}
+	};
+
+	if(button == GLUT_RIGHT_BUTTON && state == GLUT_UP){
+		PopupMenu pm;
+		for(int i = 0; i < series.size(); i++)
+			pm.append(new PopupMenuItemToggleShow(gltestp::dstring(series[i]->visible ? "Hide " : "Show ") << "[" << i << "] " << series[i]->labelname(), this, i));
+		pm.appendSeparator();
+		for(int i = 0; i < series.size(); i++)
+			pm.append(new PopupMenuItemDelete(gltestp::dstring("Delete ") << "[" << i << "] " << series[i]->labelname(), this, i));
+		pm.appendSeparator();
+		pm.append(new PopupMenuItemAddSeries<FrameTimeChartSeries>("Add frame time", this));
+		pm.append(new PopupMenuItemAddSeries<FrameRateChartSeries>("Add frame rate", this));
+		pm.append(new PopupMenuItemAddSeries<RecvBytesChartSeries>("Add received bytes", this));
+		pm.append(new PopupMenuItemAddSeries<FrameTimeHistogramChartSeries>("Add frame time histogram", this));
+		pm.append(new PopupMenuItemAddSeries<FrameRateHistogramChartSeries>("Add frame rate histogram", this));
+		pm.append(new PopupMenuItemAddSeries<RecvBytesHistogramChartSeries>("Add received bytes histogram", this));
+
+		glwPopupMenu(ws, pm);
+	}
+	return 1;
+}
+
+void GLWchart::setSeriesVisible(int i, FlagMod v){
+	if(i < 0 || series.size() <= i)
+		return;
+	series[i]->visible = !!(v & (1 << !!series[i]->visible));
+}
+
+void GLWchart::anim(double dt){
+	for(int i = 0; i < series.size(); i++){
+		if(series[i])
+			series[i]->anim(dt, this);
+	}
+}
+
+void GLWchart::draw(GLwindowState &ws, double t){
+	for(int i = 0; i < series.size(); i++){
+		ChartSeries *cs = series[i];
+		if(cs && cs->visible){
+			GLWrect cr = clientRect();
+
+			glColor4fv(cs->color());
+			glwpos2d(cr.x0, cr.y0 + (i + 2) * GLwindow::glwfontwidth * glwfontscale);
+			glwprintf(cs->labelstr());
+			glBegin(GL_LINE_STRIP);
+			int all = cs->count();
+			for(int i = 0; i < all; i++){
+				glVertex2d(cr.x0 * (all - i - 1) / all + cr.x1 * i / all, cr.y1 - (cr.height() - 2) * cs->value(i));
+			}
+			glEnd();
+		}
+	}
+}
+
+/// Define class GLWmessage for Squirrel
+bool GLWchart::sq_define(HSQUIRRELVM v){
+	st::sq_define(v);
+	sq_pushstring(v, _SC("GLWchart"), -1);
+	sq_pushstring(v, _SC("GLwindow"), -1);
+	sq_get(v, -3);
+	sq_newclass(v, SQTrue);
+	sq_settypetag(v, -1, "GLWchart");
+	sq_setclassudsize(v, -1, sizeof(WeakPtr<GLelement>));
+	register_closure(v, _SC("constructor"), sqf_constructor);
+	register_closure(v, _SC("addSeries"), sqf_addSeries);
+	sq_createslot(v, -3);
+	return true;
+}
+
+/// Squirrel constructor
+SQInteger GLWchart::sqf_constructor(HSQUIRRELVM v){
+	SQInteger argc = sq_gettop(v);
+
+	GLWchart *p = new GLWchart("Chart");
+
+	sq_assignobj(v, p);
+	glwAppend(p);
+	return 0;
+}
 
 /// Squirrel add series
 SQInteger GLWchart::sqf_addSeries(HSQUIRRELVM v){

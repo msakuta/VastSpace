738de8ebe42879faf62dadd35424baa2c61286cf
msakuta <masahiro.sakuta@gmail.com>
2013-03-08 17:51:25 +0000

    Oops, performance gap in r184 was due to my bug.  Now that the bug is fixed, clib_crc32() and clib_crc32_direct()'s performance difference is undetectable.
    git-svn-id: svn://localhost/projects/clib@190 c24b926b-5ef4-9a42-8c70-094e0699c819

--- src/crc32.c
+++ src/crc32.c
@@ -5,32 +5,34 @@
 
 static uint32_t crc_table[256];
  
-void clib_crc32_make_crc_table(void) {
-	uint32_t i = 0;
-    for (; i < 256; i++) {
-        uint32_t c = i;
-        int j = 0;
-        for (; j < 8; j++) {
-            c = (c & 1) ? (0xEDB88320 ^ (c >> 1)) : (c >> 1);
-        }
-        crc_table[i] = c;
-    }
+void clib_crc32_make_crc_table(void){
+	static int init = 0;
+	uint32_t i;
+	if(init)
+		return;
+	for(i = 0; i < 256; i++){
+		uint32_t c = i;
+		int j = 0;
+		for (; j < 8; j++) {
+			c = (c & 1) ? (0xEDB88320 ^ (c >> 1)) : (c >> 1);
+		}
+		crc_table[i] = c;
+	}
+	init = 1;
 }
 
 uint32_t clib_crc32(const uint8_t *buf, size_t len){
-	static int init = 0;
-	if(!init)
-		clib_crc32_make_crc_table();
+	clib_crc32_make_crc_table();
 	return clib_crc32_direct(buf, len);
 }
  
-uint32_t clib_crc32_direct(const uint8_t *buf, size_t len) {
-    uint32_t c = 0xFFFFFFFF;
-    size_t i = 0;
-    for (; i < len; i++) {
-        c = crc_table[(c ^ buf[i]) & 0xFF] ^ (c >> 8);
-    }
-    return c ^ 0xFFFFFFFF;
+uint32_t clib_crc32_direct(const uint8_t *buf, size_t len){
+	uint32_t c = 0xFFFFFFFF;
+	size_t i = 0;
+	for (; i < len; i++) {
+		c = crc_table[(c ^ buf[i]) & 0xFF] ^ (c >> 8);
+	}
+	return c ^ 0xFFFFFFFF;
 }
 
 const uint32_t *clib_crc32_get_crc_table(void){

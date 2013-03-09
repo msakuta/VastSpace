e12de7fa1cebfbbf8596b3c2c3c63d8968d9f7b2
msakuta <masahiro.sakuta@gmail.com>
2013-03-09 12:54:33 +0000

    Added test flag to profile random_sequence's initialization time to rseq_test.c.
    It's important to know the initialization cost as well as generation of 
    each random number.
    
    
    git-svn-id: svn://localhost/projects/clib@201 c24b926b-5ef4-9a42-8c70-094e0699c819

--- tests/rseq_test.c
+++ tests/rseq_test.c
@@ -9,14 +9,39 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+	double n = 1;
+	int test_init = 0;
 	if(argc < 2){
 		printf("usage: %s repeats\n", argv[0]);
 		printf("   Calculates file's CRC32 repeats times. Default once.\n");
 		return 1;
 	}
 	{
+		int a = 0;
+		for(a = 1; a < argc; a++){
+			if(!strcmp(argv[a], "-i")){
+				test_init = 1;
+			}
+			else
+				n = atof(argv[a]);
+		}
+	}
+	if(test_init){
 		struct random_sequence rs;
-		double i, n = atof(argv[1]);
+		double i;
+		clock_t c = clock();
+		volatile uint32_t crc = 0;
+		for(i = 0; i < n; i++){
+			init_rseq(&rs, i);
+			crc = rseq(&rs); // Make sure initialization works
+		}
+		double seconds = (double)(clock() - c) / CLOCKS_PER_SEC;
+		printf("second = %lg\n", seconds);
+		printf("initialization per second = %lg\n", i / seconds);
+	}
+	else{
+		struct random_sequence rs;
+		double i;
 		uint32_t inival;
 		clock_t c = clock();
 		volatile uint32_t crc = 0;

5fde2a08454283979eb67a04d9376c6a89e349bb
msakuta <masahiro.sakuta@gmail.com>
2013-05-15 00:48:06 +0900

    Switched to third-party resource editor for authoring .rc file.
    This is because Visual Studio 2012 Express does not come with
    the resource editor, which is needed to create or customize initial
    settings dialog with a GUI editor.
    For the sake of portability, I don't feel like relying on a specific
    platform's GUI development environment, but I cannot help using
    resource script editor for Windows right now.
    
    At least we can use freeware based workflow.  ResEdit is such a
    freeware that I ported the existing resource script to in this commit.
    
    Probably we could use open source GUI framework such as Qt.

--- src/resource.h
+++ src/resource.h
@@ -1,29 +1,17 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by e:\projects\gltestplus\gltestplus.rc
-//
-#define IDD_JOINGAME                    101
-#define IDI_ICON1                       104
-#define IDC_HOSTNAME                    1001
-#define IDC_PORT                        1002
-#define IDC_NAME                        1003
-#define IDC_LOGFILENAME                 1004
-#define IDC_LOGFILESELECT               1005
-#define IDC_PORT2                       1006
-#define IDC_MAXCLIENTS                  1006
-#define IDC_LOGFILEAPPEND               1008
-#define IDC_HOSTGAME                    1009
-#define IDC_JOINGAME                    1010
-#define IDC_STANDALONE                  1011
-#define IDC_DELETEHOST                  1012
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1012
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDI_ICON1                               100
+#define IDD_JOINGAME                            101
+#define IDC_HOSTNAME                            1001
+#define IDC_PORT                                1002
+#define IDC_NAME                                1003
+#define IDC_LOGFILENAME                         1004
+#define IDC_LOGFILESELECT                       1005
+#define IDC_MAXCLIENTS                          1006
+#define IDC_LOGFILEAPPEND                       1008
+#define IDC_HOSTGAME                            1009
+#define IDC_JOINGAME                            1010
+#define IDC_STANDALONE                          1011
+#define IDC_DELETEHOST                          1012

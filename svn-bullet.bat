9a1b15bdafbdcfaed3ef511bd9149dfb79558f63
msakuta <masahiro.sakuta@gmail.com>
2013-05-16 04:46:20 +0900

    Added VC2012 support for Bullet Dynamics Engine.
    Actually, Bullet itself does not support VC2012 yet (so do premake4)
    but it built well in my case.

--- svn-bullet.bat
+++ svn-bullet.bat
@@ -4,6 +4,9 @@ svn checkout http://bullet.googlecode.com/svn/tags/bullet-2.81 bullet
 pushd bullet\build
 
 @rem We want to use double precision
-premake4  --with-double-precision vs2008 
+premake4  --with-double-precision vs2008
+
+@rem We really need vs2012.
+premake4  --with-double-precision vs2010
 
 popd

896585cc215064ff07945a8a5a4421ee4e39310f
msakuta <masahiro.sakuta@gmail.com>
2013-05-16 03:54:51 +0900

    Added scripts to configure Bullet Dynamics Engine for building in VC2008.
    We have been importing library sources to our source tree directly
    for other libraries than Bullet, but Bullet's source is so large
    (about 8 megabytes compressed) that I hesitate to include the
    whole source into our tree.
    After all, we haven't decided whether to use BDE.
    
    Instead of embedding the whole source code, we made a script that
    checks out Bullet's Subversion working copy into a subdirectory
    and invoke its configuration manager (premake4) to match our
    demanding projects.
    
    Also we added a README that describes the method to build it.
    
    The configuration with Win32 and Debug refuses to link for unknown
    reason.

---
+++ svn-bullet.bat
@@ -0,0 +1,9 @@
+@rem First, checkout the working tree from a tag in the Subversion repository.
+svn checkout http://bullet.googlecode.com/svn/tags/bullet-2.81 bullet
+
+pushd bullet\build
+
+@rem We want to use double precision
+premake4  --with-double-precision vs2008 
+
+popd

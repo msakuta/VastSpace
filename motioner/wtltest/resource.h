1649455e77028f46844eef73bedff58cb9c6f761
msakuta <masahiro.sakuta@gmail.com>
2013-06-02 08:32:24 +0900

    motioner: Made the resource script ResEdit editable.
    Don't forget to add atlres.h to the include files in the rc.

--- motioner/wtltest/resource.h
+++ motioner/wtltest/resource.h
@@ -1,59 +1,43 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by wtltest.rc
-//
-#define IDD_ABOUTBOX                    100
-#define IDR_MAINFRAME                   128
-#define IDD_MAINDLG                     129
-#define IDD_PREVIEW                     201
-#define IDC_EDITMOVEDELTA               1000
-#define IDC_MOVEDELTA                   1000
-#define IDC_EDITROTDELTA                1001
-#define IDC_PREVIEW                     1002
-#define IDC_BONELIST                    1003
-#define IDC_OPENMODEL                   1004
-#define IDC_OPENMOTION                  1005
-#define IDC_SAVEMOTION                  1006
-#define IDC_MOTIONLIST                  1007
-#define IDC_TIMESLIDER                  1008
-#define IDC_KEYFRAMELIST                1009
-#define IDC_CURRENTFRAME                1010
-#define IDC_SCROLLBAR1                  1011
-#define IDC_POSELIST                    1011
-#define IDC_DELETEBONEPOSE              1012
-#define IDC_ADDBONEPOSE                 1013
-#define IDC_RXP                         1014
-#define IDC_RXN                         1015
-#define IDC_RYP                         1016
-#define IDC_RYN                         1017
-#define IDC_RZP                         1018
-#define IDC_RZN                         1019
-#define IDC_MXP                         1020
-#define IDC_MXN                         1021
-#define IDC_MYP                         1022
-#define IDC_MYN                         1023
-#define IDC_RZP2                        1024
-#define IDC_MZP                         1024
-#define IDC_RZP3                        1025
-#define IDC_MZN                         1025
-#define IDC_ADDKEYFRAME                 1026
-#define IDC_COMBINEDPOSE                1027
-#define IDC_INSERTKEYFRAME              1028
-#define IDC_KEYFRAMETIME                1029
-#define IDC_COPYKEYFRAME                1030
-#define IDC_PASTEKEYFRAME               1031
-#define IDC_VISIBILITY                  1032
-#define IDC_SAVEMOTION2                 1033
-#define IDC_VISIBILITY2                 1034
-#define IDC_AMPLITUDE                   1034
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        202
-#define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1032
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDD_ABOUTBOX                            100
+#define IDD_MAINDLG                             129
+#define IDD_PREVIEW                             201
+#define IDC_MOVEDELTA                           1000
+#define IDC_EDITROTDELTA                        1001
+#define IDC_PREVIEW                             1002
+#define IDC_BONELIST                            1003
+#define IDC_OPENMODEL                           1004
+#define IDC_OPENMOTION                          1005
+#define IDC_SAVEMOTION                          1006
+#define IDC_MOTIONLIST                          1007
+#define IDC_TIMESLIDER                          1008
+#define IDC_KEYFRAMELIST                        1009
+#define IDC_CURRENTFRAME                        1010
+#define IDC_POSELIST                            1011
+#define IDC_DELETEBONEPOSE                      1012
+#define IDC_ADDBONEPOSE                         1013
+#define IDC_RXP                                 1014
+#define IDC_RXN                                 1015
+#define IDC_RYP                                 1016
+#define IDC_RYN                                 1017
+#define IDC_RZP                                 1018
+#define IDC_RZN                                 1019
+#define IDC_MXP                                 1020
+#define IDC_MXN                                 1021
+#define IDC_MYP                                 1022
+#define IDC_MYN                                 1023
+#define IDC_MZP                                 1024
+#define IDC_MZN                                 1025
+#define IDC_ADDKEYFRAME                         1026
+#define IDC_COMBINEDPOSE                        1027
+#define IDC_INSERTKEYFRAME                      1028
+#define IDC_KEYFRAMETIME                        1029
+#define IDC_COPYKEYFRAME                        1030
+#define IDC_PASTEKEYFRAME                       1031
+#define IDC_VISIBILITY                          1032
+#define IDC_SAVEMOTION2                         1033
+#define IDC_AMPLITUDE                           1034
+#define IDR_MAINFRAME                           40000

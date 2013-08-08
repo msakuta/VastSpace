3ae43a1c679b8b7f6cab03123febff5298fbb758
msakuta <masahiro.sakuta@gmail.com>
2013-08-09 00:34:03 +0900

    Refactor: Removed old unused codes and include directives in Bullet.cpp.
    The codes have been came from gltest project, which had been C.
    I bet we won't revive them in the future.  We should write from scratch
    instead to reproduce similar features.

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -3,38 +3,16 @@
  */
 #include "Bullet.h"
 #include "ExplosiveBullet.h"
-#include "CoordSys.h"
-#include "astro.h"
 #include "Player.h"
 #include "judge.h"
-#include "serial_util.h"
 #ifndef DEDICATED
 #include "draw/effects.h"
 #include "draw/WarDraw.h"
 #endif
-//#include "warutil.h"
-//#include "draw/material.h"
-#include "btadapt.h"
 #include "Game.h"
 extern "C"{
-#ifndef DEDICATED
-#include "bitmap.h"
-#endif
-#include <clib/amat4.h>
-#include <clib/c.h>
 #include <clib/mathdef.h>
-#include <clib/colseq/cs.h>
-#include <clib/sound/wavemixer.h>
-#include <clib/suf/sufbin.h>
-#ifndef DEDICATED
-#include <clib/gl/gldraw.h>
-#endif
 }
-#ifndef DEDICATED
-#include <GL/glext.h>
-#endif
-#include <limits.h>
-#include <stddef.h>
 
 
 Bullet::Bullet(Entity *aowner, float alife, double adamage) : st(aowner->w), owner(aowner), damage(adamage), grav(true), life(alife), runlength(0), active(true){
@@ -77,165 +55,6 @@ void Bullet::unserialize(UnserializeContext &sc){
 	// Make the client follow the server.
 	active = true;
 }
-
-#if 0
-
-static void bullet_draw(struct bullet *, wardraw_t*);
-static void bullet_drawmodel(struct bullet *pb, wardraw_t *wd){pb,wd;}
-static int bullet_is_bullet(bullet_t *p){
-	return 1;
-}
-
-static struct bullet_static bullet_s = {
-	{NULL},
-	bullet_anim,
-	bullet_draw,
-	bullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-};
-
-
-static void nbullet_draw(struct bullet *pb, wardraw_t *wd);
-static void nbullet_drawmodel(struct bullet *pb, wardraw_t *wd);
-static void sbullet_drawmodel(struct bullet *pb, wardraw_t *wd);
-static void mortarhead_drawmodel(struct bullet *pt, wardraw_t *wd);
-static int shrapnelshell_anim(struct bullet *, warf_t *, struct tent3d_line_list *, double dt);
-static void APFSDS_draw(struct bullet *pb, wardraw_t *wd);
-static void APFSDS_drawmodel(struct bullet *pb, wardraw_t *wd);
-static int bulletswarm_anim(struct bullet *, warf_t *, struct tent3d_line_list *, double dt);
-static void bulletswarm_draw(struct bullet *, wardraw_t*);
-static void bulletswarm_drawmodel(struct bullet *pb, wardraw_t *wd);
-
-static struct bullet_static NormalBullet_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw,
-	nbullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, ShotgunBullet_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw,
-	sbullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, MortarHead_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw,
-	mortarhead_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, TracerBullet_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw,
-	nbullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, ExplosiveBullet_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw,
-	nbullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, ShrapnelShell_s = {
-	{NULL},
-	shrapnelshell_anim,
-	bullet_draw,
-	bullet_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, APFSDS_s = {
-	{NULL},
-	bullet_anim,
-	nbullet_draw/*APFSDS_draw*/,
-	APFSDS_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-}, BulletSwarm_s = {
-	{NULL},
-	bulletswarm_anim,
-	bulletswarm_draw,
-	bulletswarm_drawmodel,
-	bullet_postframe,
-	bullet_get_homing_target,
-	bullet_is_bullet,
-};
-
-typedef struct NormalBullet{
-	bullet_t st;
-	double rad;
-} NormalBullet;
-
-typedef struct TracerBullet{
-	bullet_t st;
-	double rad;
-} TracerBullet;
-
-typedef struct BulletSwarm{
-	bullet_t st;
-	double rad; /* Radius of bulletswarm */
-	double vrad; /* Velocity of radius expansion */
-	int count;
-	unsigned baf[1]; /* Bullet Availability Flags; extensible array */
-} BulletSwarm;
-
-static void BulletInit(struct bullet *pb, warf_t *w, struct bullet_static *vft){
-	pb->active = 1;
-	pb->grav = 1;
-	pb->vft = vft;
-	if(w){
-		pb->next = w->bl;
-		w->bl = pb;
-	}
-	else
-		pb->next = NULL;
-	VECNULL(pb->pos);
-	VECNULL(pb->velo);
-	VECNULL(pb->pyr);
-	VECNULL(pb->omg);
-	QUATIDENTITY(pb->rot);
-	pb->mass = 1.;
-	pb->moi = 1.;
-	pb->health = 1.;
-	pb->w = w;
-	pb->enemy = NULL;
-	pb->race = pb->shoots = pb->shoots2 = pb->kills = pb->deaths = 0;
-	pb->inputs.change = pb->inputs.press = 0;
-	pb->inputs.start = 0;
-	pb->weapon = 0;
-}
-
-struct bullet *add_bullet(warf_t *w){
-	struct bullet *pb;
-	pb = malloc(sizeof*pb);
-	BulletInit(pb, w, &bullet_s);
-	return pb;
-}
-
-struct bullet *BulletNew(warf_t *w, entity_t *owner, double damage){
-	struct bullet *pb;
-	pb = add_bullet(w);
-	pb->owner = owner;
-	pb->damage = damage;
-	pb->mass = damage;
-	pb->life = 0.;
-	return pb;
-}
-#endif
-
 
 static int bullet_hit_callback(const struct otjEnumHitSphereParam *param, Entity *pt){
 	const Vec3d *src = param->src;
@@ -307,7 +126,6 @@ bool Bullet::bulletHit(Entity *pt, WarSpace *ws, otjEnumHitSphereParam &param){
 
 			pb->pos = pos;
 			pt->onBulletHit(pb, hitpart);
-#if 1
 #ifndef DEDICATED
 			{ /* ricochet */
 				if(ws->tell && rseq(&w->rs) % (ws->effects + 1) == 0){
@@ -343,61 +161,9 @@ bool Bullet::bulletHit(Entity *pt, WarSpace *ws, otjEnumHitSphereParam &param){
 						AddTelineCallback3D(ws->tell, pos, velo, .00025 + n * .0001, quat_u, vec3_000, accel, sparkdraw, NULL, TEL3_HEADFORWARD | TEL3_REFLECT, .20 + drseq(&w->rs) * .20);
 					}
 				}
-#if 0
-					{
-						frexp(pb->damage, &n);
-						VECNORMIN(bvelo);
-						VECSCALEIN(bvelo, -(.05 + .001 * pb->damage));
-						for(j = 0; j < n; j++){
-							double velo[3], len[3];
-							GLubyte r, g, b;
-							r = 191 + rseq(&w->rs) % 64;
-							g = 128 + rseq(&w->rs) % 128;
-							b = 128 + rseq(&w->rs) % 64;
-							velo[0] = bvelo[0] * drseq(&gsrs);
-							velo[1] = bvelo[1] * drseq(&gsrs);
-							velo[2] = bvelo[2] * drseq(&gsrs);
-							VECNORM(len, velo);
-	/*						pyr[0] = atan2(velo[0], -velo[2]) + M_PI;
-							pyr[1] = atan2(velo[1], sqrt(velo[0] * velo[0] + velo[2] * velo[2]));
-							pyr[2] = 0.;*/
-							VECADDIN(velo, pt->velo);
-							AddTeline3D(tell, pos, velo, pb->damage * .0001 + .001, len, NULL, gravity,
-								COLOR32RGBA(r,g,b,255),
-								TEL3_THICK | TEL3_FADEEND, .15 + .15 * drseq(&gsrs));
-						}
-					}
-#endif
 			}
 #endif
 			damage = pb->damage;
-
-#if 0
-			{
-			extern double g_bulletimpact;
-			extern struct entity_private_static container_s;
-			if(/*0 && pt->vft == &tank_s ||*/ pt->vft == &container_s || ((struct entity_private_static*)pt->vft)->altaxis & 4){
-				double lmomentum;
-				avec3_t dr, momentum, amomentum;
-				VECSUB(dr, pb->pos, pt->pos);
-				VECSUBIN(dr, ((struct entity_private_static*)pt->vft)->cog);
-				lmomentum = g_bulletimpact * pb->mass/* * VECLEN(pb->velo)*/;
-				VECSCALE(momentum, pb->velo, lmomentum);
-				RigidAddMomentum(pt, dr, momentum);
-#if 0
-				VECSADD(pt->velo, momentum, 1. / pt->mass);
-
-				/* vector product of dr and momentum becomes angular momentum */
-				VECVP(amomentum, dr, momentum);
-				VECSADD(pt->omg, amomentum, 1. / pt->moi);
-#endif
-			}
-			}
-
-			if(hitobj)
-				*hitobj = pt;
-#endif
-#endif
 
 			if(pt->w == w) if(!pt->takedamage(this->damage, hitpart)){
 #ifndef DEDICATED
@@ -420,15 +186,6 @@ bool Bullet::bulletHit(Entity *pt, WarSpace *ws, otjEnumHitSphereParam &param){
 			extern int bullet_hits;
 			bullet_hits++;
 #endif
-
-/*			{
-				avec3_t delta;
-				VECSUB(delta, pb->pos, pt->pos);
-				VECNORM(ci.normal, delta);
-				ci.depth = 0.;
-				VECCPY(ci.velo, pt->velo);
-				bulletkill(pb, w, w->tell, avec3_000, 1, &ci);
-			}*/
 
 			return false;
 }
@@ -487,25 +244,6 @@ void Bullet::anim(double dt){
 #endif
 
 	}
-
-#if 0 // world hit
-	{
-		int i;
-		for(i = 0; i <= 4; i++){
-			avec3_t pos;
-			VECCPY(pos, pb->pos);
-			VECSADD(pos, pb->velo, dt * i / 4);
-			if(w->vft->pointhit(w, &pos, &pb->velo, dt, &ci) /*pb->pos[1] <= (w->map ? warmapheight(w->map, pb->pos[0], pb->pos[2], NULL) : 0.)*/ /*|| BULLETRANGE * BULLETRANGE < pb->pos[0] * pb->pos[0] + pb->pos[1] * pb->pos[1] + pb->pos[2] * pb->pos[2]*/){
-				static const double pyr[3] = {M_PI / 2., 0., 0.};
-				VECCPY(pb->pos, pos);
-				bulletkill(pb, w, w->tell, pyr, 1, &ci);
-				if(hitobj)
-					*hitobj = NULL;
-				return 0;
-			}
-		}
-	}
-#endif
 
 #if 0
 	if(ws) do{

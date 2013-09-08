1d8db1ad2b0fd613e6b187fc2afb54b8055a379f
msakuta <masahiro.sakuta@gmail.com>
2013-09-09 01:05:54 +0900

    surface: Make AI controlled A10 and F15 stop on the ground.
    Previously, they always try to fly no matter where they are, assuming
    there's no ground.  Now they just stop increasing throttle if on a
    ground.
    
    This behavior is useful for keeping AI controlled planes on an airport.
    
    They're not really detecting ground, but just look velocity and body
    orientation to determine whether it look like on a ground.

--- surface/A10.cpp
+++ surface/A10.cpp
@@ -221,7 +221,11 @@ void A10::anim(double dt){
 		double trim = mat.vec3(2)[1] + velo[1] + fabs(croll) * 0.05; // P + D
 		elevator = rangein(elevator + trim * dt, -1, 1);
 
-		throttle = approach(throttle, rangein((0.5 - velo.len()) * 2., 0, 1), dt, 0);
+		// If the body is stationary and upright, assume it's on the ground.
+		if(velo.slen() < 0.05 * 0.05 && 0.9 < mat.vec3(1)[1])
+			throttle = approach(throttle, 0, dt, 0);
+		else
+			throttle = approach(throttle, rangein((0.5 - velo.len()) * 2., 0, 1), dt, 0);
 	}
 
 

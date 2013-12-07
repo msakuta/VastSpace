3f64743710ee6f2578aaab0f703417e92b4ff2c3
msakuta <masahiro.sakuta@gmail.com>
2013-12-07 15:46:50 +0900

    audio: Add some Doxygen comments to playSound.h

--- include/audio/playSound.h
+++ include/audio/playSound.h
@@ -15,6 +15,15 @@ namespace audio{
 
 extern double wave_sonic_speed;
 
+/// \brief Play WAV or Ogg Vorbis format sound file.
+/// \param fileName The file name of the WAV or Ogg Vorbis file.
+/// \param delay Time delay before the sound is actually played back.
+/// \param vol Volume of the sound in range [0,256]
+/// \param pitch Sound pitch factor.  Default (as in file) is 0.  Pitch gets higher if this value is higher than 0.
+/// \param pan Pan position of the sound.
+/// \param loops Number of loops.
+/// \param priority Priority of the sound.  If added sound have higher priority and no channel is available, existing sound can be stopped.
+/// \returns Sound ID used to move or stop after this function returns.
 extern EXPORT int playSound(const char *fileName, size_t delay = 0, unsigned short vol = 256, unsigned short pitch = 0, signed char pan = 0, unsigned short loops = 0, char priority = 0);
 
 /// \brief Play WAV or Ogg Vorbis format sound file in 3-D space.
@@ -28,6 +37,7 @@ extern EXPORT int playSound(const char *fileName, size_t delay = 0, unsigned sho
 /// \returns Sound ID used to move or stop after this function returns.
 extern EXPORT int playSound3D(const char *fileName, const Vec3d &src, double vol, double attn, double delay = 0., bool loop = false, unsigned short pitch = 256);
 
+/// \brief Play raw PCM data in memory.
 extern EXPORT int playMemoryWave3D(const unsigned char *, size_t size, short pitch, const double src[3], const double org[3], const double pyr[3], double vol, double attn, double delay);
 
 }

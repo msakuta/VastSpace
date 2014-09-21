c2c170497fd56d14641b12ac86e4e772d1f1ea21
msakuta <masahiro.sakuta@gmail.com>
2014-09-21 22:16:58 +0900

    Clean up stellar_file.h by removing obsolete declarations and includes

--- include/stellar_file.h
+++ include/stellar_file.h
@@ -1,28 +1,12 @@
 #ifndef STELLAR_FILE_H
 #define STELLAR_FILE_H
 /** \file
- * \breif Functions to load Stellar Structure Definition file. */
+ * \brief Functions to load Stellar Structure Definition file. */
 
 #include "CoordSys.h"
-#include "astro.h"
-#include <stdio.h>
 #include <squirrel.h>
 #include <exception>
 
-
-
-Astrobj *new_astrobj(const char *name, CoordSys *cs);
-Astrobj *satellite_new(const char *name, CoordSys *cs);
-Astrobj *texsphere_new(const char *name, CoordSys *cs);
-Astrobj *blackhole_new(const char *name, CoordSys *cs);
-Astrobj *add_astrobj(Astrobj *a);
-
-//int StellarFileLoad(const char *fname, CoordSys *);
-
-
-void sun_draw(const struct astrobj *, const struct viewer *, int relative);
-void saturn_draw(const struct astrobj *a, const struct viewer *vw, int relative);
-void blackhole_draw(const struct astrobj *, const struct viewer*, int relative);
 
 class StellarStructureScanner;
 

eb936e78e1f8305ec0243822c58dcb9a315b8ff7
msakuta <masahiro.sakuta@gmail.com>
2015-12-26 02:13:05 -0500

    Add noise detail texture to terrainplanet like Earth
    In fact it looks better than Earth (and fast!).

--- shaders/terrainplanet.fs
+++ shaders/terrainplanet.fs
@@ -3,9 +3,15 @@ uniform int lightCount;
 #include "shaders/tonemap.fs"
 #include "shaders/shadowmap.fs"
 uniform bool shadowCast;
+uniform sampler3D noise3D;
 
 varying vec3 view;
 varying vec3 nrm;
+varying vec3 pos;
+
+// Detail texture thresholds in meters, could be uniforms
+const float detailFadeEnd = 1000.;
+const float detailFadeStart = 500.;
 
 void main (void)
 {
@@ -25,8 +31,27 @@ void main (void)
 			diffuse += ldiffuse;
 	}
 	vec4 texColor = textureCube(texture, vec3(gl_TexCoord[0]));
+
+	// Add noise as a detail texture if the fragment is close to the camera.
+	float z = -view.z;
+	if(z < detailFadeEnd){
+		// Detail texture fades out linearly.
+		// Other decay functions are possible, but it's robust and responds well.
+		// The important thing is that farther pixels won't even need to sample
+		// this texture.
+		float f = max(0, (z - detailFadeStart) / (detailFadeEnd - detailFadeStart));
+		vec3 noiseInput = pos;
+		texColor *= 1. + (0.5 * (1. - f)) * (texture3D(noise3D, 1.e5 * noiseInput)[0] - .5);
+	}
+
 	texColor.xyz *= diffuse + 0.1;
 	texColor.xyz += gl_FrontLightProduct[0].ambient.xyz;
 	texColor[3] = 1.;
 	gl_FragColor = toneMapping(texColor);
+
+	// Debug drawing the fading borders
+//	if(detailFadeEnd < z && z < detailFadeEnd * 1.01)
+//		gl_FragColor.xyz = 1.;
+//	if(detailFadeStart * 0.99 < z && z < detailFadeStart)
+//		gl_FragColor.xyz = vec3(1,0,0);
 }
--- shaders/terrainplanet.vs
+++ shaders/terrainplanet.vs
@@ -1,5 +1,6 @@
 varying vec3 view;
 varying vec3 nrm;
+varying vec3 pos;
 
 #include "shaders/shadowmap.vs"
 
@@ -9,6 +10,8 @@ void main(void)
 	view = vec3(gl_ModelViewMatrix * gl_Vertex);
 
 	nrm = gl_NormalMatrix * gl_Normal;
+
+	pos = normalize(gl_Vertex);
 
 	gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
 
